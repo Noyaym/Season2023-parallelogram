c96d0571cad3f6ae00863a45d3599dd715277326
Noyaym <noya.linder@gmail.com>
2023-02-02 12:45:57 +0200

    first commit
    Signed-off-by: Noyaym <noya.linder@gmail.com>

--- src/main/java/frc/robot/subsystems/Parallelogram.java
+++ src/main/java/frc/robot/subsystems/Parallelogram.java
@@ -1,7 +1,10 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Parallelogram extends SubsystemBase {
+    private TalonFX motor;
     
 }
